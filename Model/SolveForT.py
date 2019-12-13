ff3b596c7dccad852ff1a9e051f06af7158578af
Anna Simson <54996618+ASimson@users.noreply.github.com>
2019-12-13 09:31:57 +0100

    Commented some not applied solution methods for T Added file v_i.py

--- Model/SolveForT.py
+++ Model/SolveForT.py
@@ -11,25 +11,25 @@ hom=0 -> heterogeneous case
 !!!!!!!!!!!!!--> ONLY THE IMPLICIT SOLVER FOR HETEROGENEOUS MEDIA CAN BE USED FOR NON-UNIFORM GRIDS <------!!!!!!!!!!
 """
 import numpy as np
-from SolveDiffusionEquationHomImplicitly import solve_diff_hom_implicit
-from SolveDiffusionEquationHomExplicitly import solve_diff_hom_explicit
-from SolveDiffusionEquationHomCrankNicolson import solve_diff_hom_CN
-from SolveDiffusionEquationHetCrankNicolson import solve_diff_het_CN
+#from SolveDiffusionEquationHomImplicitly import solve_diff_hom_implicit
+#from SolveDiffusionEquationHomExplicitly import solve_diff_hom_explicit
+#from SolveDiffusionEquationHomCrankNicolson import solve_diff_hom_CN
+#from SolveDiffusionEquationHetCrankNicolson import solve_diff_het_CN
 from SolveDiffusionEquationHetImplicitly import solve_diff_het_implicit
-from SolveDiffusionEquationHetExplicitly import solve_diff_het_explicit
+#from SolveDiffusionEquationHetExplicitly import solve_diff_het_explicit
 
 def solve_for_T(T, rho_T, rho_dT, k_eff, D_eff, rhoC_eff, phi_i, v_i , nz,dt,dz, media, meth):
     
     if media=='hom':
         if meth == 'implicit':
             pass
-            (T,a,b)= solve_diff_hom_implicit(T, k_eff, D_eff, rho_dT, rhoC_eff, phi_i, dt, dz, nz)
+   #         (T,a,b)= solve_diff_hom_implicit(T, k_eff, D_eff, rho_dT, rhoC_eff, phi_i, dt, dz, nz)
         elif meth == 'explicit': 
             pass
-            (T,a,b) = solve_diff_hom_explicit(T, k_eff, D_eff, rho_dT, rhoC_eff, phi_i, dt, dz, nz)
+   #         (T,a,b) = solve_diff_hom_explicit(T, k_eff, D_eff, rho_dT, rhoC_eff, phi_i, dt, dz, nz)
         elif meth == 'CN':
             pass
-            (T,a,b) = solve_diff_hom_CN(T, k_eff, D_eff, rho_dT, rhoC_eff, phi_i, dt, dz, nz) 
+    #        (T,a,b) = solve_diff_hom_CN(T, k_eff, D_eff, rho_dT, rhoC_eff, phi_i, dt, dz, nz) 
         else:
             print('requested method for homogeneous temperature equation not available, check input')            
             
@@ -39,10 +39,10 @@ def solve_for_T(T, rho_T, rho_dT, k_eff, D_eff, rhoC_eff, phi_i, v_i , nz,dt,dz,
             (T,a,b) = solve_diff_het_implicit(T, rho_T, rho_dT, k_eff, D_eff, rhoC_eff, phi_i, dt, dz, v_i, nz)
         elif meth == 'explicit':  
             pass
-            (T,a,b) = solve_diff_het_explicit(T, rho_dT, k_eff, D_eff, rhoC_eff, phi_i, dt, dz, nz)
+  #          (T,a,b) = solve_diff_het_explicit(T, rho_dT, k_eff, D_eff, rhoC_eff, phi_i, dt, dz, nz)
         elif meth == 'CN':
             pass
-            (T,a,b) = solve_diff_het_CN(T, rho_dT, k_eff, D_eff, rhoC_eff, phi_i, dt, dz, nz)
+   #         (T,a,b) = solve_diff_het_CN(T, rho_dT, k_eff, D_eff, rhoC_eff, phi_i, dt, dz, nz)
         else:
             print('requested method for heteroeneous temperature equation not available, check input') 
 

23896d639fbd27c4c99da49d80a0bab9db5ec822
Anna Simson <54996618+ASimson@users.noreply.github.com>
2020-06-15 08:48:24 +0200

    added docstrings to functions

--- Model/SetUpTime.py
+++ Model/SetUpTime.py
@@ -1,27 +1,52 @@
-"""
-set_up_iter : defined the time step for the first iteration and sets total simulation time at start to 0 s
-t_total     : calculates total simulation time for each iteration by adding up all time steps
-comp_dt     : calculates the time step for the next iteration based on mesh fourier number with coefficients a and b from temperature equation and computes the stability criterion 
-=========  ========
-t          maximum simulation time
-dt         time step [s]
-dt0        time step for first iteration [s]
-iter_max   maximum number of time steps 
-t_passed   simulation time passed [s]
-SC         stability criterion
-"""
 import numpy as np
+
+
 def set_up_iter(it):
+    '''
+    defined the time step for the first iteration and sets total simulation time at start to 0 s
+
+    Arguments:
+    ---------------
+    it  number of iterations
+
+    Results:
+    ---------------
+    dt0 first time step
+    t_passed initialize time control
+
+    '''
     iter_max = it
     dt0 = 0.01
     t_passed = 0
     return iter_max, dt0, t_passed
 
 def t_total(t_passed,dt):
+    '''
+    calculates total simulation time for each iteration by adding up all time steps
+    Arguments
+    --------------
+    t_passed    time passed
+    dt          time step
+    '''
     return t_passed +dt
 
 
 def comp_dt(t_passed,dz, a,beta):
+    '''
+    calculates the time step for the next iteration based on mesh fourier number with coefficients a and b from 
+    temperature equation and computes the stability criterion 
+    Arguments:
+    ----------------
+    t_passed    time already passed
+    dz          space discretization
+    a           parameter from temperature equation
+    beta        parameter from temperature equation
+
+    Results:
+    ---------------
+    dt          new time step
+    SC          Stability criterion has to be below 0.5
+    '''
     nz = np.size(a)
     D = np.zeros(nz)
     SC = np.zeros(nz)

Sign error corrected

Model/SolveForC.py
@@ -3,7 +3,6 @@ import numpy as np
 def solve_for_c(T, T_prev, phi_i, k_eff, rhoC_eff, D_eff, rho_T, rho_dT, v_i, nz, dt, dz):
     """
     Computes condensation rate c [kg/m^3/s]
-
     EQU : 72 from Hansen -> c = d/dz(D_eff * rho_dT dT/dz) - (1-phi_i) *rho_dT * dT/dt [- rho_v^eq * nabla (phi_i * v_i)] <- last term in square brackets results from incorporation of settling velocity
 
     ============  ===============================
@@ -67,15 +66,15 @@ def solve_for_c(T, T_prev, phi_i, k_eff, rhoC_eff, D_eff, rho_T, rho_dT, v_i, nz
     a = (1-phi_i) *rho_dT/dt
 # Set up matrix H
     #Matrix elements 
-    main_H[1:-1] = (a[1:-1]  + 0.5/((dz[1:]**2 + dz[:-1]**2)/2)* (beta[:-2] + 2*beta[1:-1] + beta[2:]))
+    main_H[1:-1] = -(a[1:-1]  + 0.5/((dz[1:]**2 + dz[:-1]**2)/2)* (beta[:-2] + 2*beta[1:-1] + beta[2:]))
     lower_H[:-1] = 0.5/((dz[1:]**2 + dz[:-1]**2)/2) * (beta[1:-1] + beta[:-2])
     upper_H[1:] = 0.5/((dz[1:]**2 + dz[:-1]**2)/2) * (beta[1:-1]+ beta[2:])
-    # Insert Boundary Condition
+    # Insert entries for boundary nodes
     lower_H[-1] = 0 #0.5/dz[-1]**2*(beta[-1] + beta[-1]) -0.5/dz[-1]**2*(2*beta[-1])
     upper_H[0] =  0 #-0.5/dz[0]**2 * beta[0]  +0.5/dz[0]**2 * (beta[0] + beta[0]) #1
-    main_H[0] =  - a[0]  #1# -2/(dz[0]**2)* (beta[0] ) + 2* 1/(dz[0]**2) * beta[0] - a[0]
-    main_H[-1] = - a[-1]  # 1#0.5/(dz[-1]**2)* (4*beta[-1]) - 2* 1/(dz[-1]**2) * (beta[-1] ) -a[-1]   
-    H = np.diag(np.ones(nz)*(-main_H),k=0) +np.diag(np.ones(nz-1)*(lower_H),k=-1) + np.diag(np.ones(nz-1)*(upper_H),k=1)
+    main_H[0] =  -a[0]  #1# -2/(dz[0]**2)* (beta[0] ) + 2* 1/(dz[0]**2) * beta[0] - a[0]
+    main_H[-1] = -a[-1]  # 1#0.5/(dz[-1]**2)* (4*beta[-1]) - 2* 1/(dz[-1]**2) * (beta[-1] ) -a[-1]   
+    H = np.diag(np.ones(nz)*(main_H),k=0) +np.diag(np.ones(nz-1)*(lower_H),k=-1) + np.diag(np.ones(nz-1)*(upper_H),k=1)
 
 # Set up matrix G
  #Matrix elements
@@ -97,10 +96,8 @@ def solve_for_c(T, T_prev, phi_i, k_eff, rhoC_eff, D_eff, rho_T, rho_dT, v_i, nz
     lower_E[:-1] = -factor_dz * beta_right
     upper_E[1:] = factor_dz * beta_left
     
-    E = np.diag(np.ones(nz)*(main_E),k=0) +np.diag(np.ones(nz-1)*(lower_E),k=-1) + np.diag(np.ones(nz-1)*(upper_E),k=1)
-    
+    E = np.diag(np.ones(nz)*(main_E),k=0) +np.diag(np.ones(nz-1)*(lower_E),k=-1) + np.diag(np.ones(nz-1)*(upper_E),k=1) 
     FD_error = np.dot(E,T)
-
     c = c + FD_error 
 
 #%% Term from settling velocity p_v^eq *d/dz (phi_i v_i)
@@ -117,10 +114,8 @@ def solve_for_c(T, T_prev, phi_i, k_eff, rhoC_eff, D_eff, rho_T, rho_dT, v_i, nz
 
     F = np.diag(np.ones(nz)*(main_F),k=0 )+np.diag(np.ones(nz-1)*(lower_F),k=-1) + np.diag(np.ones(nz-1)*(upper_F),k=1) 
     velterm = np.dot(F, vphi)
-    
     c = c - velterm
-    
-    
+   
     return c
 
 
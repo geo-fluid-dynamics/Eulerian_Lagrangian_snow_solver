e789e7d3d21d3fb6e7afe6ff72f93c2e6382d1b3
Anna Simson <54996618+ASimson@users.noreply.github.com>
2020-07-13 08:48:26 +0200

    Adjusted all changes in the main file, changed naming of the generated file

--- Model/MainSnowModel.py
+++ Model/MainSnowModel.py
@@ -1,3 +1,19 @@
+from ModelGeometry import set_up_model_geometry
+from SetUpTime import set_up_iter, t_total, comp_dt
+from SetUpInitialConditions import initial_conditions
+from ModelParameter import model_parameters
+from SolveDiffusionEquationHetImplicitly import solve_diff_het_implicit
+from TempGradient import solve_for_grad_T
+from StoreResults import store_results, set_up_matrixes
+from VisualizeResults import visualize_results
+from SolveForC import solve_for_c 
+from coupled_update_phi_coord import coupled_update_phi_coord
+from phi_from_rho_eff import fractions
+from BoundaryCondition import boundary_condition
+from plot_eta import eta_plot
+from velocity import settling_vel
+import matplotlib.pyplot as plt 
+import numpy as np
 """
  ==========  =================================================================
  Name        Description
@@ -33,76 +49,58 @@ sigma         vertical stress acting from the overlaying snow at snow height z [
 Matrices for storing results of each iteration
 all_xy         
 
- """
+"""
 
 
-from SetUpModelGeometry import set_up_model_geometry 
-from SetUpTime import set_up_iter, t_total, comp_dt
-from SetUpInitialConditions import initial_conditions
-from ModelParameter import model_parameters
-from SolveDiffusionEquationHetImplicitly import solve_diff_het_implicit
-from TempGradient import solve_for_grad_T
-from StoreResults import store_results, set_up_matrixes
-from VisualizeResults import visualize_results
-from SolveForC import solve_for_c 
-from retrieve_phi import solve_for_phi
-from phi_from_rho_eff import fractions
-from BoundaryCondition import boundary_condition
-from velocity import settling_vel
-import matplotlib.pyplot as plt 
-
-import numpy as np
-
-def main_snow_model(geom = 5, RHO = 6, TT = 3, SWVD = 'Loewe', SetVel = 'Y'):
+def main_snow_model(geom = 'FieldScale0.5m', RHO_ini = 'RHO_2Layer_Continuous_smooth', T_ini = 'T_const_263', SWVD = 'Libbrecht', SetVel = 'Y', v_opt = 'continuous' , viscosity = 'eta_phiT', it = 5793):
     '''
     main snow model
     '''
-    [nz, dz, Z, Z_ini, coord] = set_up_model_geometry(SetVel, geom)
-    [iter_max, dt, t_passed] = set_up_iter(36*24*5+2)  
-    [T, rho_eff] = initial_conditions(nz, Z, RHO, TT)
+    [nz, dz, Z, coord] = set_up_model_geometry(geom)
+    [iter_max, dt, t_passed] = set_up_iter(it)  
+    [T, rho_eff] = initial_conditions(nz, Z, RHO_ini, T_ini)
     mass_flux = []
 
-#    T = boundary_condition(T)
+    T = boundary_condition(T)
     phi = fractions (nz,rho_eff) 
     [all_D_eff, all_k_eff, all_SC, all_rhoC_eff, all_rho_T, all_T,all_c, all_phi,all_grad_T,all_rho_eff,all_coord, all_v, all_sigma, all_t_passed,all_dz] = set_up_matrixes(iter_max, nz)
     SC = np.zeros(nz)
-    c = np.ones(nz) *0
-    [D_eff, k_eff, rhoC_eff, rho_T, rho_dT] = model_parameters(phi, T, Z, nz, coord, SWVD)
-    [v, v_dz, sigma] = settling_vel(T, nz, coord, phi, SetVel)
+    c = np.zeros(nz)
+    [D_eff, k_eff, rhoC_eff, rho_T, rho_dT] = model_parameters(phi, T, nz, coord, SWVD)
+    [v, v_dz, sigma] = settling_vel(T, nz, coord, phi, SetVel, v_opt, viscosity)
     for t in range(iter_max):
         print(t)
         grad_T = solve_for_grad_T(T, dz, nz)
         [all_D_eff, all_k_eff, all_SC, all_rhoC_eff, all_rho_T, all_T,all_c,all_phi, all_grad_T, all_rho_eff, all_coord, all_v, all_sigma, all_t_passed,  all_dz] \
         =  store_results(all_D_eff, all_k_eff, all_SC, all_rhoC_eff, all_rho_T, all_T, all_c,all_phi,all_grad_T, all_rho_eff, all_coord, all_v, all_sigma, all_t_passed,all_dz, D_eff, k_eff, SC, phi, rhoC_eff, rho_T, T, c, grad_T, rho_eff, coord, v, sigma,  t, iter_max, nz,dz,t_passed)
         T_prev = T
-      #  (T, a, b) = solve_diff_het_implicit(T, rho_T,rho_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz)
-      #  c = solve_for_c(T, T_prev, phi, k_eff, rhoC_eff, D_eff, rho_T, rho_dT, v, nz, dt, dz)
-        (phi, coord, Z, dz, v_dz, v, sigma) = solve_for_phi(T, c, dt, nz, phi, v_dz, coord, SetVel)
-        [D_eff, k_eff, rhoC_eff, rho_T, rho_dT] = model_parameters(phi, T, Z, nz, coord, SWVD)
+        (T, a, b) = solve_diff_het_implicit(T, rho_T,rho_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz)
+        c = solve_for_c(T, T_prev, phi, k_eff, rhoC_eff, D_eff, rho_T, rho_dT, v, nz, dt, dz)
+        (phi, coord, dz, v_dz, v, sigma) = coupled_update_phi_coord(T, c, dt, nz, phi, v_dz, coord, SetVel, v_opt, viscosity)
+        [D_eff, k_eff, rhoC_eff, rho_T, rho_dT] = model_parameters(phi, T, nz, coord, SWVD)
         t_passed = t_total(t_passed,dt)
-        # if t_passed > 3600*24*2+2:
+        # if t_passed > 3600*24*4+2:
         #     print(t)
         #     break
 
-        dt = 100
-        #[dt, SC] = comp_dt(t_passed,dz, a,b)
-        mass_flux_dt = c * dt
-        for x in range(len(mass_flux_dt)):
-            if mass_flux_dt[x] >0:
-                mass_flux.append(mass_flux_dt[x])
+        #dt = 100
+        [dt, SC] = comp_dt(t_passed,dz, a,b)
 
-    # np.savetxt('all_phi_v_eta(phi)', all_phi)
-    # np.savetxt('all_coord_v_eta(phi)', all_coord)
-    # np.savetxt('all_v_v_eta(phi)', all_v)
-    # np.savetxt('all_dz_v_eta(phi)', all_dz)
-    # np.savetxt('all_c_v_eta(phi)', all_c)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_phi'      , all_phi)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_coord'    , all_coord)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_t_passed' , all_t_passed)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_v'        , all_v)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_dz'       , all_dz)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_c'        , all_c)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_T'        , all_T)
 
     print(sum(mass_flux))
 
 
 ### Visualize results
-    visualize_results(all_T, all_c, all_phi, all_grad_T, all_rho_eff, all_SC, all_coord, all_v, all_sigma, iter_max, nz, Z, dt, all_dz,all_t_passed,  plot=True)
-
+    visualize_results( all_T, all_c, all_phi, all_grad_T, all_rho_eff, all_SC, all_coord, all_v, all_sigma, iter_max, nz, Z, dt, all_dz,all_t_passed, geom, RHO_ini, T_ini, SWVD , SetVel , v_opt, viscosity, plot=True)
+    if viscosity is not 'eta_constant':
+        eta_plot(all_T, all_phi, all_coord, all_t_passed, iter_max,  geom, RHO_ini, T_ini, SWVD , SetVel , v_opt, viscosity)
     
     return all_T, all_D_eff, all_SC, all_rho_T, all_k_eff, all_c, all_phi, all_grad_T, all_rho_eff, all_coord,all_v, all_sigma, all_t_passed, all_dz
 

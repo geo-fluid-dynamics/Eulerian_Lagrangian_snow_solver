40e916aab16b74ce58da5cec962352b446ca2ceb
Anna Simson <54996618+ASimson@users.noreply.github.com>
2020-11-24 09:13:45 +0100

    changed names

--- Model/StoreResults.py
+++ Model/StoreResults.py
@@ -13,23 +13,23 @@ def set_up_matrixes (iter_max,nz):
     ----------
     storage matrices filled with zeros
     '''
-    all_N = np.zeros([iter_max,nz])
+    all_CFL = np.zeros([iter_max,nz])
     all_T = np.zeros([iter_max,nz])
-    all_rho_T = np.zeros([iter_max,nz])
+    all_rho_v = np.zeros([iter_max,nz])
     all_D_eff = np.zeros([iter_max,nz])
     all_k_eff = np.zeros([iter_max,nz])
     all_rhoC_eff = np.zeros([iter_max,nz])
-    all_phi_i = np.zeros([iter_max,nz])
+    all_phi = np.zeros([iter_max,nz])
     all_c = np.zeros([iter_max,nz])
     all_rho_eff = np.zeros([iter_max,nz])
     all_coord = np.zeros([iter_max,nz])
-    all_v_i = np.zeros([iter_max,nz])
+    all_v = np.zeros([iter_max,nz])
     all_sigma = np.zeros([iter_max,nz])
     all_t_passed = np.zeros(iter_max)
     all_dz = np.zeros([iter_max,nz-1])
-    return all_D_eff, all_k_eff, all_N, all_rhoC_eff, all_rho_T, all_T,all_c,all_phi_i, all_rho_eff, all_coord, all_v_i, all_sigma, all_t_passed, all_dz
+    return all_D_eff, all_k_eff, all_CFL, all_rhoC_eff, all_rho_v, all_T,all_c,all_phi, all_rho_eff, all_coord, all_v, all_sigma, all_t_passed, all_dz
 
-def store_results(all_D_eff, all_k_eff, all_N, all_rhoC_eff, all_rho_T, all_T, all_c, all_phi_i, all_rho_eff, all_coord, all_v_i, all_sigma, all_t_passed, all_dz, D_eff, k_eff, N, phi_i, rhoC_eff, rho_T, T, c, rho_eff,coord, v_i, sigma, t, iter_max, nz,dz,t_passed):
+def store_results(all_D_eff, all_k_eff, all_CFL, all_rhoC_eff, all_rho_v, all_T, all_c, all_phi, all_rho_eff, all_coord, all_v, all_sigma, all_t_passed, all_dz, D_eff, k_eff, CFL, phi, rhoC_eff, rho_v, T, c, rho_eff,coord, v, sigma, t, iter_max, nz,dz,t_passed):
     '''
     fill storage matrices with values computed in the current time step
 
@@ -43,16 +43,16 @@ def store_results(all_D_eff, all_k_eff, all_N, all_rhoC_eff, all_rho_T, all_T, a
     '''
     all_D_eff[t,:] = D_eff
     all_k_eff[t,:] = k_eff
-    all_N[t,:] = N
-    all_phi_i[t,:] = phi_i
+    all_CFL[t,:] = CFL
+    all_phi[t,:] = phi
     all_rhoC_eff[t,:] = rhoC_eff
-    all_rho_T[t,:] = rho_T
+    all_rho_v[t,:] = rho_v
     all_T[t,:] = T
     all_c[t,:] = c
     all_rho_eff[t,:] = rho_eff
     all_coord[t,:] = coord
-    all_v_i[t,:] = v_i
+    all_v[t,:] = v
     all_sigma[t,:] = sigma
     all_t_passed[t] = t_passed
     all_dz[t,:] = dz
-    return all_D_eff, all_k_eff, all_N, all_rhoC_eff, all_rho_T, all_T,all_c,all_phi_i, all_rho_eff, all_coord, all_v_i, all_sigma, all_t_passed, all_dz
+    return all_D_eff, all_k_eff, all_CFL, all_rhoC_eff, all_rho_v, all_T, all_c,all_phi, all_rho_eff, all_coord, all_v, all_sigma, all_t_passed, all_dz

7a7a6ef67a0c859baf2e35021604bb88e2bab15e
Anna Simson <54996618+ASimson@users.noreply.github.com>
2020-11-24 09:18:53 +0100

    changed names

--- Model/MainSnowModel.py
+++ Model/MainSnowModel.py
@@ -13,7 +13,7 @@ from velocity import settling_vel
 import matplotlib.pyplot as plt 
 import numpy as np
  
-def main_snow_model(geom = 'FieldScale0.5m', RHO_ini = 'RHO_2Layer_Continuous_smooth', T_ini = 'T_const_263', SWVD = 'Libbrecht', SetVel = 'Y', v_opt = 'continuous' , viscosity = 'eta_phiT', it = 172220):
+def main_snow_model(geom = 'FieldScale0.5m', RHO_ini = 'RHO_2Layer_Continuous_smooth', T_ini = 'T_const_273', SWVD = 'Libbrecht', SetVel = 'Y', v_opt = 'continuous' , viscosity = 'eta_constant', it = 2000):
     '''
     main snow model
     
@@ -22,60 +22,60 @@ def main_snow_model(geom = 'FieldScale0.5m', RHO_ini = 'RHO_2Layer_Continuous_sm
     geom            geometry (height, number of cells) of the initial snowpack 'FieldScale0.5m' (our approach 0.5 m snow heigth) , 'LabScale0.02m' (our approach 0.02  snow height) , 'Crocus0.5m_2Layer' (for comparison with settling in crocus)
     RHO_ini         initial density of the snowpack
     T_ini           initial temperature of the snowpack
-    SWD             Equation for Saturation water vapor density
+    SWD             equation for Saturation water vapor density
     SetVel          if settling yes: 'Y' if no: 'N'
     v_opt           velocity option either 'continuous' (our approach) or 'crocus'
     viscosity       option to compute viscosity 'eta_constant', 'eta_phi', 'eta_phiT', 'eta_T'   
     it              maximum number of iterations
+    
+    Returns
+    -------------
     '''
 
     [nz, dz, Z, coord] = set_up_model_geometry(geom)
     [iter_max, dt, t_passed] = set_up_iter(it)  
     [T, rho_eff] = initial_conditions(nz, Z, RHO_ini, T_ini)
     mass_flux = []
-
-    T = boundary_condition(T)
-    phi = fractions (nz,rho_eff) 
-    [all_D_eff, all_k_eff, all_SC, all_rhoC_eff, all_rho_T, all_T,all_c, all_phi,all_rho_eff,all_coord, all_v, all_sigma, all_t_passed,all_dz] = set_up_matrixes(iter_max, nz)
-    SC = np.zeros(nz)
+  #  T = boundary_condition(T)
+    phi = fractions (nz,rho_eff)
+    [all_D_eff, all_k_eff, all_CFL, all_rhoC_eff, all_rho_v, all_T,all_c, all_phi,all_rho_eff,all_coord, all_v, all_sigma, all_t_passed,all_dz] = set_up_matrixes(iter_max, nz)
+    CFL = np.zeros(nz)
     c = np.zeros(nz)
-    [D_eff, k_eff, rhoC_eff, rho_T, rho_dT] = model_parameters(phi, T, nz, coord, SWVD)
+    [D_eff, k_eff, rhoC_eff, rho_v, rho_v_dT] = model_parameters(phi, T, nz, coord, SWVD)
     [v, v_dz, sigma] = settling_vel(T, nz, coord, phi, SetVel, v_opt, viscosity)
     for t in range(iter_max):
         print(t)
-        [all_D_eff, all_k_eff, all_SC, all_rhoC_eff, all_rho_T, all_T,all_c,all_phi,  all_rho_eff, all_coord, all_v, all_sigma, all_t_passed,  all_dz] \
-        =  store_results(all_D_eff, all_k_eff, all_SC, all_rhoC_eff, all_rho_T, all_T, all_c,all_phi, all_rho_eff, all_coord, all_v, all_sigma, all_t_passed,all_dz, D_eff, k_eff, SC, phi, rhoC_eff, rho_T, T, c, rho_eff, coord, v, sigma,  t, iter_max, nz,dz,t_passed)
+        [all_D_eff, all_k_eff, all_CFL, all_rhoC_eff, all_rho_v, all_T,all_c,all_phi,  all_rho_eff, all_coord, all_v, all_sigma, all_t_passed,  all_dz] \
+        =  store_results(all_D_eff, all_k_eff, all_CFL, all_rhoC_eff, all_rho_v, all_T, all_c,all_phi, all_rho_eff, all_coord, all_v, all_sigma, all_t_passed,all_dz, D_eff, k_eff, CFL, phi, rhoC_eff, rho_v, T, c, rho_eff, coord, v, sigma,  t, iter_max, nz,dz,t_passed)
         T_prev = T
-        (T, a, b) = solve_diff_het_implicit(T, rho_T,rho_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz)
-        c = solve_for_c(T, T_prev, phi, k_eff, rhoC_eff, D_eff, rho_T, rho_dT, v, v_dz, nz, dt, dz, t)
-        (phi, coord, dz, v_dz, v, sigma) = coupled_update_phi_coord(T, c, dt, nz, phi, v_dz, coord, SetVel, v_opt, viscosity)
-        [D_eff, k_eff, rhoC_eff, rho_T, rho_dT] = model_parameters(phi, T, nz, coord, SWVD)
+        (T, a, b) = solve_diff_het_implicit(T, rho_v,rho_v_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz)      # solves for temperature - Diffusion requires CFL
+        c = solve_for_c(T, T_prev, phi, k_eff, rhoC_eff, D_eff, rho_v, rho_v_dT, v, v_dz, nz, dt, dz)        # solves for condensation rate - Diffusion
+        (phi, coord, dz, v_dz, v, sigma) = coupled_update_phi_coord(T, c, dt, nz, phi, v_dz, coord, SetVel, v_opt, viscosity)   #solves for ice volume fraction and coordinates update - Advection does not require CFL
+        [D_eff, k_eff, rhoC_eff, rho_v, rho_v_dT] = model_parameters(phi, T, nz, coord, SWVD)
         t_passed = t_total(t_passed,dt)
-        # if t_passed > 3600*24*4+2:
-        #     print(t)
-        #     break
+        # if t_passed > 3600*48:
+        #     k=5
+      # dt = 100
+        [dt, CFL] = comp_dt(t_passed,dz, a,b)  
 
-       # dt = 100
-        [dt, SC] = comp_dt(t_passed,dz, a,b)
-
-    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_phi'      , all_phi)
-    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_coord'    , all_coord)
-    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_t_passed' , all_t_passed)
-    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_v'        , all_v)
-    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_dz'       , all_dz)
-    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_c'        , all_c)
-    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + str(it) + '_all_T'        , all_T)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + '_' + str(it) + '_all_phi'      , all_phi)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + '_' + str(it) + '_all_coord'    , all_coord)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + '_' + str(it) + '_all_t_passed' , all_t_passed)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + '_' + str(it) + '_all_v'        , all_v)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + '_' + str(it) + '_all_dz'       , all_dz)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + '_' + str(it) + '_all_c'        , all_c)
+    np.savetxt( str(geom) + '_' + str(RHO_ini) + '_' + str(T_ini) + '_' + 'Vel_' + str(v_opt)  + '_' + str(viscosity) + '_' + str(it) + '_all_T'        , all_T)
 
     print(sum(mass_flux))
 
 
 ### Visualize results
-    visualize_results( all_T, all_c, all_phi, all_rho_eff, all_SC, all_coord, all_v, all_sigma, iter_max, nz, Z, dt, all_dz,all_t_passed, geom, RHO_ini, T_ini, SWVD , SetVel , v_opt, viscosity, plot=True)
+    visualize_results( all_T, all_c, all_phi, all_rho_eff, all_CFL, all_coord, all_v, all_sigma, iter_max, nz, Z, dt, all_dz,all_t_passed, geom, RHO_ini, T_ini, SWVD , SetVel , v_opt, viscosity, plot=True)
 
     
-    return all_T, all_D_eff, all_SC, all_rho_T, all_k_eff, all_c, all_phi,  all_rho_eff, all_coord,all_v, all_sigma, all_t_passed, all_dz
+    return all_T, all_D_eff, all_CFL, all_rho_v, all_k_eff, all_c, all_phi,  all_rho_eff, all_coord,all_v, all_sigma, all_t_passed, all_dz
 
-all_T, all_D_eff, all_SC, all_rho_T, all_k_eff, all_c, all_phi,  all_rho_eff, all_coord, all_v, all_sigma, all_t_passed, all_dz= main_snow_model()
+all_T, all_D_eff, all_CFL, all_rho_v, all_k_eff, all_c, all_phi,  all_rho_eff, all_coord, all_v, all_sigma, all_t_passed, all_dz= main_snow_model()
     
         
         

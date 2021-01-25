1d6d35e52b93a9fa6def748f8f9e375feaa51bf2
Anna Simson <simson@aices.rwth-aachen.de>
2021-01-25 19:38:12 +0100

    adjusted CFL to MFN, u to T, u_new to T_new

--- Model/T_solver.py
+++ Model/T_solver.py
@@ -1,7 +1,7 @@
 from boundary_conditions import set_boundary_conditions
 import numpy as np
 from constant_variables import L
-def solve_for_T(u, rho_v, rho_v_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz):
+def solve_for_T(T, rho_v, rho_v_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz):
      '''
      (rhoC_eff+(1-phi)*rho_v_dT * L)* dT/dt = (L* D_eff* rho_v_dT + k-eff)* d^2T/dz^2
      Solves 1D diffusion equation for heterogeneous media with implicit method 
@@ -10,7 +10,7 @@ def solve_for_T(u, rho_v, rho_v_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz):
 
      Arguments
      ----------
-          u         state variable to be solved for (in this case T) 
+          T         temperature of previous time step [K] 
           rho_v     saturation water vapor density   [kgm-3s-1]
           rho_v_dT  derivative of rho_v w.r.t. T 
           k_eff     thermal conductivity       [Wm-1K-1]
@@ -24,9 +24,9 @@ def solve_for_T(u, rho_v, rho_v_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz):
 
      Returns
      ----------
-          u         updated state variable
-          a         coefficient required to determine CFL condition
-          beta      coefficient required to determine CFL condition
+          T_new     updated temperature [K]
+          a         coefficient required to determine mesh fourier number
+          beta      coefficient required to determine mesh fourier number
 
      '''
      # Initialize variables    
@@ -75,14 +75,14 @@ def solve_for_T(u, rho_v, rho_v_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz):
      main_A[-1] = 1
      lower_A [-1] = 0
 
-### Set up tridiagonal Matrix A and solve for new u  
+### Set up tridiagonal Matrix A and solve for new T  
      A = np.zeros([nz,nz])          
      A = np.diag(np.ones(nz)*main_A,k=0) +np.diag(np.ones(nz-1)*lower_A,k=-1) +\
      np.diag(np.ones(nz-1)*upper_A,k=1)
      
 ## Set up tridiagonal matrix B (RHS)     
      B = np.diag(np.ones(nz)*(a),k=0)
-     b = u
+     b = T
      b = np.dot(B,b)
      
 ### Apply boundary condition
@@ -96,8 +96,8 @@ def solve_for_T(u, rho_v, rho_v_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz):
      lower_E[:-1] = -factor_dz * beta_right
      upper_E[1:] = factor_dz * beta_left     
      E = np.diag(np.ones(nz)*(main_E),k=0) +np.diag(np.ones(nz-1)*(lower_E),k=-1) + np.diag(np.ones(nz-1)*(upper_E),k=1)  
-     FD_error = np.dot(E,u)
-     b = b - FD_error    
+     FD_error = np.dot(E,T)
+     b -= FD_error    
      
 #%% Not considered in the paper! Set up matrix C - account for settling velocity
      # vphi = phi*v     
@@ -112,5 +112,5 @@ def solve_for_T(u, rho_v, rho_v_dT, k_eff, D_eff, rhoC_eff, phi, v, nz, dt, dz):
      # velterm = np.dot(C, vphi)          
      # b = b - velterm
 
-     u_new = np.linalg.solve(A,b)
-     return u_new,  a, beta
+     T_new = np.linalg.solve(A,b)
+     return T_new,  a, beta

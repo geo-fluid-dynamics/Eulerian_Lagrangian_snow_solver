751804c03c8e4a0009834a6a5b8268edd94407c0
Anna Simson <simson@mbd.rwth-aachen.de>
2025-05-20 11:47:40 +0200

    udate t_solver name

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,7 @@ from model.geometry import set_up_model_geometry
 from model.set_time import set_up_iter, t_total, comp_dt
 from model.initial_conditions import set_initial_conditions
 from model.parameters import update_model_parameters
-from model.T_solver import solve_for_T
+from model.t_solver import solve_for_T
 from model.store import store_results, set_up_matrices
 from model.visualize_results import plot_results
 from model.c_solver import solve_for_c 
